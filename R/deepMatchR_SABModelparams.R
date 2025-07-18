1fbf8311773859141abd8c7fc6d1660597f05864
theHumanBorch <ncborch@gmail.com>
2025-07-18 09:11:45 -0500

    Add SAB model params
    # 3 - Header order can be found in the params themselves

---
+++ R/deepMatchR_SABModelparams.R
@@ -0,0 +1,8 @@
+#' Data transformation recipe for Class I and Class MFI values, including the
+#' method and for each allele, the ceiling, mu (mean), and standard deviation
+#' values. 
+#' @name deepMatchR_SABModelparams
+#' @docType data
+#'
+#'
+NULL
